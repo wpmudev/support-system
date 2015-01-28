91b905297f3671f349fe6048d7817345d570fd07
igmoweb <igmoweb@gmail.com>
2015-01-28 10:36:19 +0100

    Welcome screen adapted to 2.1 version

--- admin/views/welcome.php
+++ admin/views/welcome.php
@@ -15,7 +15,7 @@
 	<div class="changelog">
 		<div class="feature-section col one-col">
 			<div class="col-1">
-				<h2><?php _e( 'Support System has been revamped', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h2>
+				<h2><?php _e( 'FAQs in Frontend are finally here!', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h2>
 			</div>
 		</div>
 	</div>
@@ -23,8 +23,9 @@
 	<div class="changelog">
 		<div class="feature-section col two-col">
 			<div class="col-1">
-				<h3><?php _e( 'New frontend!', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h3>
-				<p><?php printf( __( 'Support System comes with <a href="%s">Foundation 5</a> integrated. Foundation 5 is a consistent and bullet-proof CSS Framework that allows to display the front in the best way possible in most of the themes.', INCSUB_SUPPORT_LANG_DOMAIN ), 'http://foundation.zurb.com/' ); ?></p>
+				<p><?php printf( __( 'Support System comes now with options to display a FAQ in your site. As we did with tickets, FAQs are rendered using <a href="%s">Foundation 5</a>.', INCSUB_SUPPORT_LANG_DOMAIN ), 'http://foundation.zurb.com/' ); ?></p>
+				<p><?php _e( 'You can manage your FAQs from admin, as usual. Users will still have access to FAQs through an admin menu unless you want to deactivate that menu in Settings', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
+				<p><?php _e( 'FAQs system comes with <strong>Pro Sites</strong> integration. Allow any kind of Pro User with any level assigned to see your FAQs!', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
 			</div>
 			<div class="col-2 last-feature">
 				<img src="<?php echo INCSUB_SUPPORT_PLUGIN_URL . '/admin/assets/images/support-welcome-1.png'; ?>">
@@ -36,18 +37,21 @@
 
 		<div class="feature-section col three-col">
 			<div>
-				<h4><?php esc_html( _e( 'Totally recoded', INCSUB_SUPPORT_LANG_DOMAIN ) ); ?></h4>
-				<p><?php _e( 'Yes. We have completely refurbished home. More hooks, a better code, faster and more readable is now populating the plugin core.', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
+				<h4><?php esc_html_e( 'Changes in admin styles', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h4>
+				<img src="<?php echo INCSUB_SUPPORT_PLUGIN_URL . '/admin/assets/images/support-welcome-2.png'; ?>">
+				<p><?php _e( 'We did it again. Support System 2.1 comes with many little (and bigger) improvements in admin. We have remade the Edit Ticket Screen thinking on usability, now you will be able to manage your tickets in a single page.', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
 			</div>
 			<div>
-				<h4><?php esc_html( _e( 'Extensibility is the key', INCSUB_SUPPORT_LANG_DOMAIN ) ); ?></h4>
+				<h4><?php esc_html_e( 'Widgets in Frontend Tickets', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h4>
+				<img src="<?php echo INCSUB_SUPPORT_PLUGIN_URL . '/admin/assets/images/support-welcome-3.png'; ?>">
 				<p><?php _e( 'We believe in making Support System extensible so you or your developer can add more and more features easily to the plugin.', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
 			</div>
 			<div class="last-feature">
-				<h4><?php esc_html( _e( 'Better performance', INCSUB_SUPPORT_LANG_DOMAIN ) ); ?></h4>
-				<p><?php _e( 'Support System 2.0 comes with an improved performance in all areas. Results are now cached for a better response and queries have been recoded from scratch', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
+				<h4><?php esc_html_e( 'More speed!', INCSUB_SUPPORT_LANG_DOMAIN ); ?></h4>
+				<p><?php _e( 'Cache system has been reviewed, Support System now makes less queries and it behaves faster than ever!', INCSUB_SUPPORT_LANG_DOMAIN ); ?></p>
 			</div>
 		</div>
+
 
 		<hr>
 
@@ -58,3 +62,4 @@
 	</div>
 
 </div>
+
