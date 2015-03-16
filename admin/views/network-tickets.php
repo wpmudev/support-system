a68807185f4c3e30784e892517bcb62fe48f2907
igmoweb <igmoweb@gmail.com>
2015-03-16 10:50:03 +0100

    Added double confirmation before deleting a ticket

--- admin/views/network-tickets.php
+++ admin/views/network-tickets.php
@@ -2,3 +2,12 @@
 	<?php include( 'ticket-status-links.php' ); ?>
 	<?php include( 'tickets-table-form.php' ); ?>
 </div>
+
+<script>
+	jQuery(document).ready(function($) {
+		$( 'span.delete > a' )
+			.click( function(e) {
+				return confirm( '<?php _e( "Are you sure you want to delete this ticket?", INCSUB_SUPPORT_LANG_DOMAIN ); ?>');
+			})
+	});
+</script>
