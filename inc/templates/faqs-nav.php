8c0ff509555b4b49cd499f3e90ee6207a2074ec0
igmoweb <igmoweb@gmail.com>
2015-05-14 12:29:01 +0200

    - Fixed: Query class remade. FAQs, Tickets and Ticket Form can now live in the same page.

--- inc/templates/faqs-nav.php
+++ inc/templates/faqs-nav.php
@@ -6,7 +6,7 @@
 					<?php incsub_support_the_faq_category_filter( 'cat-id' ); ?>
 				</div>
 				<div class="large-4 columns">
-					<?php incsub_support_the_search_input( array( 'placeholder' => __( 'Search FAQs', INCSUB_SUPPORT_LANG_DOMAIN ) ) ); ?>
+					<?php incsub_support_the_search_input( array( 'placeholder' => __( 'Search FAQs', INCSUB_SUPPORT_LANG_DOMAIN ), 'type' => 'faq' ) ); ?>
 				</div>
 				<div class="large-3 columns">
 					<input type="submit" class="button small" name="support-system-submit-filter" value="<?php esc_attr_e( 'Filter' , INCSUB_SUPPORT_LANG_DOMAIN ); ?>"/>
