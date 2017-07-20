cc5099061b06083a96265330f4a747c941190dbb
igmoweb <igmoweb@gmail.com>
2017-07-20 11:40:40 +0200

    Improved Grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -149,6 +149,22 @@ module.exports = function(grunt) {
         }
     });
 
+    grunt.registerTask( 'finish', function() {
+        var json = grunt.file.readJSON('package.json');
+        var file = './build/' + json.name + '-' + json.version + '.zip';
+        grunt.log.writeln( 'Process finished. Browse now to: ' + json.projectEditUrl['green'].bold );
+        grunt.log.writeln( 'And upload the zip file under: ' + file['green'].bold);
+        grunt.log.writeln('----------');
+        grunt.log.writeln('');
+        grunt.log.writeln( 'Remember to tag this new version:' );
+
+        var tagMessage = 'git tag -a ' + json.version + ' -m "$CHANGELOG"';
+        var pushMessage = 'git push -u origin ' + json.version;
+        grunt.log.writeln( tagMessage['green'] );
+        grunt.log.writeln( pushMessage['green'] );
+        grunt.log.writeln('----------');
+    });
+
     grunt.loadNpmTasks('grunt-search');
 
     grunt.registerTask('version-compare', [ 'search' ] );
--- README.md
+++ README.md
@@ -22,8 +22,10 @@ To avoid this while developing set `define('SCRIPT_DEBUG', true);` in your wp-co
 
 
 # Before releasing
-1. `gulp release` will minify the CSS and JS.
-2. `npm run build` will create a package in build folder. Make sure that your package.json `pluginVersion` matches with the plugin version in main file.
+1. Make sure that the version in `incsub-support.php` matches with the version in `package.json`, otherwise the build will fail.
+2. Checkout `master` and merge `development` by using `git merge development`
+3. Now execute `npm run release`. A new folder called `build` will be created where you can grab the zip file for the new version. Follow instructions to generate a new Git tag and upload the file to WPMU DEV.
+4. Language files, JS Lint and text domains verification are done during the execution of this script so developer doesn't need to worry about these tasks.
 
 
 
